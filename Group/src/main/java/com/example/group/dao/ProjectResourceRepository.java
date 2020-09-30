40b4d8221c1c34ad6e80db27cbaee7765b3fd143
cartgo <43975440+cartgo@users.noreply.github.com>
2020-09-30 15:46:24 -0400

    push failed this moring,push again..

--- Group/src/main/java/com/example/group/dao/ProjectResourceRepository.java
+++ Group/src/main/java/com/example/group/dao/ProjectResourceRepository.java
@@ -1,2 +1,14 @@
-package com.example.group.dao;public interface ProjectResourceRepository {
+package com.example.group.dao;
+
+import com.example.group.model.Project;
+import com.example.group.model.ProjectResource;
+import com.example.group.model.ProjectScope;
+import com.example.group.model.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+
+public interface ProjectResourceRepository  extends JpaRepository<ProjectResource,Integer> {
+    @Transactional
+    void deleteProjectResourceByProjectAndResource(Project project , Resource resource);
 }
