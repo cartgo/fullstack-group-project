535a5b76d9476b01ccaa50803737a56ec26519e9
Yichun Liu <yl6343@nyu.com>
2020-10-02 14:44:48 -0400

    resource folder

--- Group/src/main/java/com/example/group/controller/ResourceController.java
+++ Group/src/main/java/com/example/group/controller/ResourceController.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/resource")
 public class ResourceController {
