216757c5682376448658014e7700bd687657b698
cartgo <43975440+cartgo@users.noreply.github.com>
2020-10-05 13:48:42 -0400

    projectScope revise,add method revise, id datatype changed

--- Group/src/main/java/com/example/group/dao/ProjectScopeRepository.java
+++ Group/src/main/java/com/example/group/dao/ProjectScopeRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectScopeRepository extends  JpaRepository<ProjectScope,String> {
+public interface ProjectScopeRepository extends  JpaRepository<ProjectScope,Integer> {
     ProjectScope findByItemId(String itemId);
     ProjectScope save(ProjectScope projectScope);
     /*@Transactional
