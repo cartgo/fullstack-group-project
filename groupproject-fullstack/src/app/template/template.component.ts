8d01235e97dba071ff0b58e1c541d2144f91273c
Harshini Shivakumar <harshini.shivakumar@gmail.com>
2020-10-08 12:38:20 -0400

    Harshini

--- groupproject-fullstack/src/app/template/template.component.ts
+++ groupproject-fullstack/src/app/template/template.component.ts
@@ -12,24 +12,31 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class TemplateComponent implements OnInit {
 
-
+ // displayArray:Array<string>;
   displayArray= new Array();
+  uniqueArray= new Array();
   selectedType = '';
   colForm: FormGroup;
   template: Template[];
+  getColumnData: string[];
 
-  constructor(private fb: FormBuilder, private formulaService:FormulaService) { }
+  constructor(private fb: FormBuilder, private formulaService: FormulaService) { }
 
   ngOnInit() {
     this.colForm = this.fb.group({
       columns: this.fb.array([this.getcolumn()])
-    })
+    });
+
+    this.formulaService.getColumn().subscribe((data) => {
+      //console.log(data);
+      this.getColumnData = data;
+    });
 
   }
 
   get columnForms() {
     return this.colForm.get('columns') as FormArray
-    console.log(this.colForm.get('columns'));
+    //console.log(this.colForm.get('columns'));
   }
 
   private getcolumn() {
@@ -62,36 +69,39 @@ export class TemplateComponent implements OnInit {
       //let t=column.columnType;
       //let temp:Template;
       //temp=column;
+      if(column.columnName!=null && column.columnType!=null)
+      {
       console.log(column.columnName,column.columnType,column.formula);
+      this.displayArray.push(column.columnName);
       this.formulaService.addColumn(column);
-      console.log(this.displayArray);
-     // window.location.replace('/search/formula');
+      }
     }
+    this.uniqueArray = [...new Set(this.displayArray)];
+    console.log(this.uniqueArray);
+    this.formulaService.testMethod(this.uniqueArray);
+   
+    //window.location.replace('/search/formula');
   }
  
-  name(event){
-    this.displayArray.push("name");
-    console.log(event.checked)
+
+  dynamicCheck(data,event){
+    if(event.checked==true){
+    this.displayArray.push(data);
     console.log(this.displayArray);
+    }
+    if(event.checked==false){
+      const index = this.displayArray.indexOf(data);
+      if (index > -1) {
+      this.displayArray.splice(index, 1);
+        }
+      //this.displayArray.pop();
+      console.log(this.displayArray)
+    }
   }
 
-  cost_code(event){
-    this.displayArray.push("cost_code");
-    console.log(event.checked)
-    console.log(this.displayArray);
-  }
-
-  editable(event){
-    this.displayArray.push("editable");
-    console.log(event.checked)
-    console.log(this.displayArray);
-  }
-
-  item_id(event){
-    this.displayArray.push("item_id");
-    console.log(event.checked)
-    console.log(this.displayArray);
-  }
+  //get columnsToDisplay(){
+  //  return this.uniqueArray;
+ // }
  
 
 }
