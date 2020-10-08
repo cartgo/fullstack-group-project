66fcfcfe0eed9c8193245ef3c2edc2381e8e1926
Harshini Shivakumar <harshini.shivakumar@gmail.com>
2020-10-08 12:34:58 -0400

    Harshini

--- Group/src/main/java/com/example/group/dao/DatabaseUpdates.java
+++ Group/src/main/java/com/example/group/dao/DatabaseUpdates.java
@@ -3,6 +3,8 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class DatabaseUpdates{
@@ -20,4 +22,39 @@ public class DatabaseUpdates{
 
         entityManager.createNativeQuery(query).executeUpdate();
     }
+
+    @Transactional
+    public void updateIntRecord(String tableName, int value,
+
+                                        String columnName, int item_id) {
+
+        String query = "UPDATE `" + tableName + "` SET " + columnName + "=" + value + " WHERE item_id=" + item_id;
+              //  + "` " +
+               // columnType;
+
+        entityManager.createNativeQuery(query).executeUpdate();
+    }
+
+    @Transactional
+    public void updateStringRecord(String tableName, String value,
+
+                                String columnName, int item_id) {
+
+        String query = "UPDATE `" + tableName + "` SET " + columnName + "= '" + value + "' WHERE item_id=" + item_id;
+        //  + "` " +
+        // columnType;
+
+        entityManager.createNativeQuery(query).executeUpdate();
+    }
+
+    //select distinct column_name from information_schema.columns where table_name = 'project_scope';
+
+    @Transactional
+    public List<String> displayColumns(String tableName) {
+
+        String query = "SELECT DISTINCT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name= '" + tableName +"'";
+
+        return entityManager.createNativeQuery(query).getResultList();
+    }
+
 }
